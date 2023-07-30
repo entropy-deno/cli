681e33e494191ff6a9f4464fc43cd876795101d8
dominiq007 <dom.rajkowski@gmail.com>
2023-07-30 19:58:49 +0200

    chore: bump std version

--- src/entropy.ts
+++ src/entropy.ts
@@ -1,4 +1,4 @@
-import { parse as parseFlags } from 'https://deno.land/std@0.194.0/flags/mod.ts';
+import { parse as parseFlags } from 'https://deno.land/std@0.196.0/flags/mod.ts';
 import { inject, Logger } from 'https://deno.land/x/entropy@1.0.0-alpha.2/src/mod.ts';
 
 if (import.meta.main) {
