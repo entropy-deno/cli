f951ff9366efe0ba99d4b01a5372c983278315e6
dominiq007 <dom.rajkowski@gmail.com>
2023-09-27 20:55:08 +0200

    chore: bump std version

--- entropy.ts
+++ entropy.ts
@@ -1,4 +1,4 @@
-import { parse as parseFlags } from 'https://deno.land/std@0.202.0/flags/mod.ts';
+import { parse as parseFlags } from 'https://deno.land/std@0.203.0/flags/mod.ts';
 import { Constructor } from 'https://deno.land/x/entropy@1.0.0-beta.1/src/utils/utils.module.ts';
 import { inject } from 'https://deno.land/x/entropy@1.0.0-beta.1/src/injector/injector.module.ts';
 import { Logger } from 'https://deno.land/x/entropy@1.0.0-beta.1/src/logger/logger.module.ts';
--- init.ts
+++ init.ts
@@ -1,4 +1,4 @@
-import { parse as parseFlags } from 'https://deno.land/std@0.202.0/flags/mod.ts';
+import { parse as parseFlags } from 'https://deno.land/std@0.203.0/flags/mod.ts';
 import { NewCommand } from './src/commands/new.command.ts';
 
 const flags = parseFlags(Deno.args, {
